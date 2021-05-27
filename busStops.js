795bb70cbf99c8775c3377adef24916d0384124c
Suwanna Truss <suwanna.truss@softwire.com>
2021-05-27 17:51:05 +0100

    end of day1

--- busStops.js
+++ busStops.js
@@ -14,19 +14,24 @@ const geoLocation = await fetch(`https://api.postcodes.io/postcodes/${postcode}`
  const lat = geoLocation.result.latitude;
  const lon = geoLocation.result.longitude;
  
-// get nearest busstops in radius 500m
+// get nearest busStops in radius 500m from Tfl API
 const nearestBusStops = await fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${lat}&lon=${lon}&stopTypes=NaptanBusWayPoint%2CNaptanBusCoachStation%2CNaptanOnstreetBusCoachStopPair%2CNaptanPrivateBusCoachTram%2C%20NaptanPublicBusCoachTram%2CNaptanOnstreetBusCoachStopCluster&radius=500`) 
     .then(response => response.json())
    // .then(body => console.log(body))
     .catch(err => console.log(err));
+const stopPoint1 = nearestBusStops.stopPoints[0];
 
-
-const nextArrivalBusStop1 = await fetch(`https://api.tfl.gov.uk/StopPoint/${stopPoint}/Arrivals`) 
+console.log(stopPoint1.commonName)
+console.log(stopPoint1.naptanId)
+const naptanId = '490008660N'  //stopPoint1.naptanId
+// get next arrival bus from 
+const nextArrivalBusStop1 = await fetch(`https://api.tfl.gov.uk/StopPoint/${naptanId}/Arrivals`) 
      .then(response => response.json())
     //  .then(body => console.log(body))
      .catch(err => console.log(err));
     // console.log(output);
-const sortedByArrivalTimeStopPoint = nextArrivalBusStop1.sort(function(busA, busB) {
+console.log(nextArrivalBusStop1)
+    const sortedByArrivalTimeStopPoint = nextArrivalBusStop1.sort(function(busA, busB) {
           return busA.timeToStation - busB.timeToStation
      });
 
@@ -37,6 +42,6 @@ for (let i =0; i < 5; i++) {
     console.log('in '+ Math.ceil(sortedByArrivalTimeStopPoint[i].timeToStation/60) + ' mins');
     console.log('in'+ Math.round(sortedByArrivalTimeStopPoint[i].timeToStation/60) + 'mins');
     }
-    
+
 }
  run()
