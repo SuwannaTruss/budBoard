147cbb53844d530f9b7cde02b62b842f64141319
Suwanna Truss <suwanna.truss@softwire.com>
2021-05-28 16:34:53 +0100

    end of day2

--- busStops.js
+++ busStops.js
@@ -5,47 +5,65 @@ async function run() {
     // get userinput - postcode    
     const postcode = prompt('Please input postcode');
 
-    // get lat/lon from postcode API
-    const geoLocation = await fetch(`https://api.postcodes.io/postcodes/${postcode}`)
-        .then(response => response.json())
-        //  .then(body => console.log(body))
-        .catch(err => console.log(err));
-
-    const lat = geoLocation.result.latitude;
-    const lon = geoLocation.result.longitude;
-
-    // get nearest busStops in radius 500m from Tfl API
-    const nearestBusStops = await fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${lat}&lon=${lon}&stopTypes=NaptanPublicBusCoachTram&radius=500`)
-        .then(response => response.json())
-        // .then(body => console.log(body))
-        .catch(err => console.log(err));
-        https://api.tfl.gov.uk/StopPoint/?lat=51.5843&lon=-0.145149&stopTypes=NaptanPublicBusCoachTram&radius=500
-        //
-  
-
-    for (let i = 0; i < 2; i++) {
-        const naptanId = nearestBusStops.stopPoints[i].naptanId;
-        // const naptanId = '490008660N'  
-        console.log('Bus stop name: '+nearestBusStops.stopPoints[i].commonName+ ' , '+nearestBusStops.stopPoints[i].indicator)
-
-        // get next arrival bus from 
-        const nextArrivalBusStop1 = await fetch(`https://api.tfl.gov.uk/StopPoint/${naptanId}/Arrivals`)
+    try {
+        // get lat/lon from postcode API
+        const geoLocation = await fetch(`https://api.postcodes.io/postcodes/${postcode}`)
             .then(response => response.json())
             //  .then(body => console.log(body))
             .catch(err => console.log(err));
-        // console.log(output);
-        //console.log(nextArrivalBusStop1)
-        const sortedByArrivalTimeStopPoint = nextArrivalBusStop1.sort(function (busA, busB) {
-            return busA.timeToStation - busB.timeToStation
-        });
 
-        for (let i = 0; i < 5; i++) {
-            console.log('\nBus: ' + (i + 1));
-            console.log('route:' + sortedByArrivalTimeStopPoint[i].lineName);
-            console.log('destination: ' + sortedByArrivalTimeStopPoint[i].destinationName);
-            console.log('in ' + Math.ceil(sortedByArrivalTimeStopPoint[i].timeToStation / 60) + ' mins');
+        const lat = geoLocation.result.latitude;
+        const lon = geoLocation.result.longitude;
+
+        // get nearest busStops in radius 500m from Tfl API
+        const nearestBusStops = await fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${lat}&lon=${lon}&stopTypes=NaptanPublicBusCoachTram&radius=100`)
+            .then(response => response.json())
+            // .then(body => console.log(body))
+            .catch(err => console.log(err));
+        
+            console.log(nearestBusStops.stopPoints)
+
+        if (nearestBusStops.stopPoints.length >= 2) {
+            for (let i = 0; i < 2; i++) {
+                const naptanId = nearestBusStops.stopPoints[i].naptanId;
+                console.log('Bus stop name: ' + nearestBusStops.stopPoints[i].commonName + ' , ' + nearestBusStops.stopPoints[i].indicator)
+
+                // get next arrival bus from 
+                const nextArrivalBusStop = await fetch(`https://api.tfl.gov.uk/StopPoint/${naptanId}/Arrivals`)
+                    .then(response => response.json())
+                    //  .then(body => console.log(body))
+                    .catch(err => console.log(err));
+                // console.log(output);
+                //console.log(nextArrivalBusStop)
+
+                if (nextArrivalBusStop) {
+                    const sortedByArrivalTimeStopPoint = nextArrivalBusStop.sort(function (busA, busB) {
+                        return busA.timeToStation - busB.timeToStation
+                    });
+
+                    for (let i = 0; i < 5; i++) {
+                        if (sortedByArrivalTimeStopPoint[i]) {
+                            console.log('Bus: ' + (i + 1));
+                            console.log('route:' + sortedByArrivalTimeStopPoint[i].lineName);
+                            console.log('destination: ' + sortedByArrivalTimeStopPoint[i].destinationName);
+                            console.log('in ' + Math.ceil(sortedByArrivalTimeStopPoint[i].timeToStation / 60) + ' mins\n');
+                        } 
+
+                    }
+                } else {
+                    console.log('There are no buses coming at this stop.')
+                }
+            }
+        } else {
+            console.log('There are no nearby bus stops.')
         }
 
+    } catch (error) {
+        console.log('Invalid London postcode, please try again.');
+        const rerun = prompt('Type 1 to try again ');
+         if (rerun === "1") {
+             run();
+         }
     }
 }
 run()
