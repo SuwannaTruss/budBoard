02a7650a5997950b0f30f0bfbd53e0b6e397729c
Suwanna Truss <suwanna.truss@softwire.com>
2021-05-29 18:23:01 +0100

    part2 - completed

--- busStops.js
+++ busStops.js
@@ -1,18 +1,16 @@
 const fetch = require('node-fetch')
 const prompt = require('prompt-sync')();
-
+// logger 
 const winston = require('winston');
 const logger = winston.createLogger({
     levels: winston.config.syslog.levels,
     transports: [
-    //   new winston.transports.Console({ level: 'error' }),
       new winston.transports.File({
         filename: 'combined.log',
         level: 'info'
       })
     ]
   })
-
 
 async function run() {
     // get userinput - postcode    
@@ -22,7 +20,6 @@ async function run() {
         // get lat/lon from postcode API
         const geoLocation = await fetch(`https://api.postcodes.io/postcodes/${postcode}`)
             .then(response => response.json())
-            //  .then(body => console.log(body))
             .catch(err => console.log(err));
 
         const lat = geoLocation.result.latitude;
@@ -31,7 +28,6 @@ async function run() {
         // get nearest busStops in radius 500m from Tfl API
         const nearestBusStops = await fetch(`https://api.tfl.gov.uk/StopPoint/?lat=${lat}&lon=${lon}&stopTypes=NaptanPublicBusCoachTram&radius=500`)
             .then(response => response.json())
-            // .then(body => console.log(body))
             .catch(err => console.log(err));
         
         if (nearestBusStops.stopPoints.length >= 2) {
@@ -39,19 +35,18 @@ async function run() {
                 const naptanId = nearestBusStops.stopPoints[i].naptanId;
                 console.log('Bus stop name: ' + nearestBusStops.stopPoints[i].commonName + ' , ' + nearestBusStops.stopPoints[i].indicator)
 
-                // get next arrival bus from 
+                // get next arrival bus from the 2 closest stops using NaptanId
                 const nextArrivalBusStop = await fetch(`https://api.tfl.gov.uk/StopPoint/${naptanId}/Arrivals`)
                     .then(response => response.json())
-                    //  .then(body => console.log(body))
                     .catch(err => console.log(err));
-                // console.log(output);
-                //console.log(nextArrivalBusStop)
 
+                // sorting buses by arrival time
                 if (nextArrivalBusStop.length > 0) {
                     const sortedByArrivalTimeStopPoint = nextArrivalBusStop.sort(function (busA, busB) {
                         return busA.timeToStation - busB.timeToStation
                     });
 
+                    // Display details of the next 5 buses to arrive
                     for (let i = 0; i < 5; i++) {
                         if (sortedByArrivalTimeStopPoint[i]) {
                             console.log('Bus: ' + (i + 1));
@@ -61,16 +56,18 @@ async function run() {
                         } 
 
                     }
+                // log to console and logger that there are no buses arriving at the stop
                 } else {
                     console.log('There are no buses coming at this stop.')
                     logger.log('info', `no buses arriving at this stop - Naptan ID: ${naptanId} at ${timeStamp}`)
                 }
             }
+        // log to console and logger that there are no nearby bus stops to a valid postcode
         } else {
             console.log('There are no nearby bus stops.')
             logger.log('info', `Valid postcode, no nearby bus stops, user input: ${postcode}`)
         }
-
+    // catch an error if the postcode entered is invalid & invite user to give correct postcode
     } catch (error) {
         console.log( 'Invalid London postcode, please try again.');
         logger.log('error',`Invalid London postcode, user input: ${postcode}`);
